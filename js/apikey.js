f2807e63b1987d82e39171fad050cdb66ad41b43
hongbiKo <rhghdql666@gmail.com>
2022-10-18 00:45:10 +0900

    [TEST]

---
+++ js/apikey.js
@@ -0,0 +1,3 @@
+const config = {
+  apikey: "6b63d0783533c92f7fd8cbf963fc1d0e",
+};
--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,5 @@
 "use strict";
-
+const API_KEY = config.apikey;
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
